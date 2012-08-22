e454f35859a0237a0fd56292fa2f57d3b3c071d0
deaths_soul (MCP) <NULL>
2012-08-22 14:03:52 -0400

    Adding an additional check to cut down on loop runs somewhat.

--- src/fileUtil.cpp
+++ src/fileUtil.cpp
@@ -7,15 +7,15 @@ void util::createMast(parser::fileFormat::file fileA, std::vector<parser::fileFo
 	for(unsigned int i = 0; i < fileA.groups.length(); ++i){
 		for(unsigned int j = 0; j < fileA.groups[i].records.length(); ++j){
 			for(unsigned int k = 0; k < masters.groups.length(); ++k){
-				if(!test){
-					for(unsigned int l = 0; l < masters.groups[k].records.length(); ++l){
-						if(fileA.groups[i].records[j].ID == masters.groups[k].records[l].ID){
-							ids += fileA.groups[i].records[j].ID;
-							test = true;
-							break;
-						}
+				for(unsigned int l = 0; l < masters.groups[k].records.length(); ++l){
+					if(fileA.groups[i].records[j].ID == masters.groups[k].records[l].ID){
+						ids += fileA.groups[i].records[j].ID;
+						test = true;
+						break;
 					}
 				}
+				if(test)
+					break;
 			}
 			test = false;
 		}
