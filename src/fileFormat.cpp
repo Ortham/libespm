bb53a44b5b1dd479a7e7b7334913377c18d59d23
deaths_soul (MCP) <NULL>
2012-09-27 17:33:15 -0400

    Cleaning out some stuff and changing the name of the project for the purposes of the GPL.

--- src/fileFormat.cpp
+++ src/fileFormat.cpp
@@ -1,21 +1,21 @@
 /*
  * fileFormat.cpp
- * This file is part of Parse
+ * This file is part of libespm
  *
- * Copyright (C) 2012 - deaths_soul(MCP)
+ * Copyright (C) 2012 - deaths_soul (MCP)
  *
- * Parse is free software: you can redistribute it and/or modify
+ * libespm is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * Parse is distributed in the hope that it will be useful,
+ * libespm is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with Parse. If not, see <http://www.gnu.org/licenses/>.
+ * along with libespm. If not, see <http://www.gnu.org/licenses/>.
  */
  
 /**
--- src/fileUtil.cpp
+++ src/fileUtil.cpp
@@ -1,3 +1,23 @@
+/*
+ * fileUtil.cpp
+ * This file is part of libespm
+ *
+ * Copyright (C) 2012 - deaths_soul (MCP)
+ *
+ * libespm is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * libespm is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with libespm. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include <string>
 #include "fileUtil.h"
 void util::createMast(parser::fileFormat::file &fileA){
