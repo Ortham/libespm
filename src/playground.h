54d0a1c90ced739eb8e668d1cf8169d14bfef2d5
WrinklyNinja <wrinklyninja1@gmail.com>
2013-05-06 13:36:23 +0100

    Implemented very lightweight parser for BOSSv3, just gets TES4 header and vector of FormIDs in plugin. Also implemented a YAML-based settings file and class so that no global vars are necessary.

---
+++ src/playground.h
@@ -0,0 +1,337 @@
+/*
+ * playground.h
+ * This file is part of libespm
+ *
+ * Copyright (C) 2013 WrinklyNinja
+ *
+ * libespm is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * libespm is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with libespm. If not, see <http://www.gnu.org/licenses/>.
+ */
+#ifndef __PLAYGROUND__
+#define __PLAYGROUND__
+
+#include <string>
+#include <vector>
+#include <stdint.h>
+#include <fstream>
+
+#include <iostream>
+
+#include <yaml-cpp/yaml.h>
+#include <zlib.h>
+
+//Libespm playground for BOSSv3. Useful things will be implemented properly into my libespm fork.
+//Try to get full plugin data read implemented with reasonable performance (speed/memory) impact - mem usage of 2x plugin filesizes is upper limit.
+//Also test for Oblivion, FO3 and FNV plugins, needs to work for those too.
+
+namespace espm {
+    
+
+    struct Settings {
+        Settings() {}
+        Settings(const std::string& filepath, const std::string& game) {
+            
+            YAML::Node contents = YAML::LoadFile(filepath);  //Throws exception on failure.
+
+            YAML::Node ys = contents[game];
+
+            length = ys["Length"].as<unsigned int>();
+            field_size_length = ys["FieldSizeLength"].as<unsigned int>();
+            flag_length = ys["FlagLength"].as<unsigned int>();
+            comp_flag = ys["CompFlag"].as<uint32_t>();
+            mast_flag = ys["MastFlag"].as<uint32_t>();
+
+            if (ys["IDLength"])
+                id_length = ys["IDLength"].as<unsigned int>();
+            if (ys["RevLength"])
+                rev_length = ys["RevLength"].as<unsigned int>();
+            if (ys["VerLength"])
+                ver_length = ys["VerLength"].as<unsigned int>();
+            if (ys["StuffzLength"])
+                unknown_length = ys["StuffzLength"].as<unsigned int>();
+            if (ys["HeadSizeLength"])
+                head_size_length = ys["HeadSizeLength"].as<unsigned int>();
+            if (ys["RecSizeLength"])
+                rec_size_length = ys["RecSizeLength"].as<unsigned int>();
+            if (ys["DecompSizeLength"])
+                decomp_size_length = ys["DecompSizeLength"].as<unsigned int>();
+            if (ys["GroupTypeLength"])
+                group_type_length = ys["GroupTypeLength"].as<unsigned int>();
+            if (ys["GroupStampLength"])
+                group_stamp_length = ys["GroupStampLength"].as<unsigned int>();
+            if (ys["GroupSizeLength"])
+                group_size_length = ys["GroupSizeLength"].as<unsigned int>();
+            
+            bsa_head = ys["BSAHead"].as<std::string>();
+            plug_head = ys["PlugHead"].as<std::string>();
+            mast_ext = ys["MastExt"].as<std::string>();
+            plug_ext = ys["PlugExt"].as<std::string>();
+            save_ext = ys["SaveExt"].as<std::string>();
+            group = ys["Group"].as<std::string>();
+            if (ys["SaveHead"])
+                save_head = ys["SaveHead"].as<std::string>();
+            
+            onam = ys["ONAM"].as< std::vector<std::string> >();
+        }
+        
+        unsigned int length;
+        unsigned int field_size_length;
+        unsigned int flag_length;
+        uint32_t comp_flag;
+        uint32_t mast_flag;
+        unsigned int id_length;
+        unsigned int rev_length;
+        unsigned int ver_length;
+        unsigned int unknown_length;
+        unsigned int head_size_length;
+        unsigned int rec_size_length;
+        unsigned int decomp_size_length;
+        unsigned int group_type_length;
+        unsigned int group_stamp_length;
+        unsigned int group_size_length;
+
+        std::string bsa_head;
+        std::string plug_head;
+        std::string save_head;
+        std::string mast_ext;
+        std::string plug_ext;
+        std::string save_ext;
+        std::string group;
+
+        std::vector<std::string> onam;
+    };
+
+    struct Field {
+        Field() : data(NULL) {}
+        Field(const Field& field) {
+            dataSize = field.dataSize;
+            data = new char[dataSize];
+            memcpy(type, field.type, 4);
+            memcpy(data, field.data, dataSize);
+        }
+        
+        ~Field() {
+            delete [] data;
+        }
+
+        Field& operator = (Field const& rhs) {
+            if (data != rhs.data) {
+                delete [] data;
+
+                dataSize = rhs.dataSize;
+                data = new char[dataSize];
+                memcpy(type, rhs.type, 4);
+                memcpy(data, rhs.data, dataSize);
+            }
+
+            return *this;
+        }
+        
+        char type[4];
+
+        uint16_t dataSize;
+
+        char * data;
+
+        uint32_t Read(std::ifstream &input, const Settings& settings) {
+
+            input.read(type, settings.length);
+            input.read(reinterpret_cast<char*>(&dataSize), settings.field_size_length);
+
+            data = new char[dataSize];
+            input.read(data, dataSize);
+
+            return (uint32_t)settings.length + settings.field_size_length + dataSize;
+        }
+
+        uint32_t Read(char * buffer, const Settings& settings) {
+
+            memcpy(type, buffer, settings.length);
+            memcpy(&dataSize, buffer + settings.length, settings.field_size_length);
+
+            data = new char[dataSize];
+            memcpy(data, buffer + settings.length + settings.field_size_length, dataSize);
+            
+            return (uint32_t)settings.length + settings.field_size_length + dataSize;
+        }
+    };
+
+    struct Record {
+        char type[4];
+        
+        uint32_t dataSize;
+        uint32_t flags;
+        uint32_t id;
+        uint32_t revision;
+        
+        uint16_t version;
+        uint16_t unknown;
+
+        std::vector<Field> fields;
+
+        uint32_t Read(std::ifstream &input, const Settings& settings) {
+            input.read(type, settings.length);
+            input.read(reinterpret_cast<char*>(&dataSize), settings.head_size_length);
+            input.read(reinterpret_cast<char*>(&flags), settings.flag_length);
+            input.read(reinterpret_cast<char*>(&id), settings.id_length);
+            input.read(reinterpret_cast<char*>(&revision), settings.rev_length);
+            input.read(reinterpret_cast<char*>(&version), settings.ver_length);
+            input.read(reinterpret_cast<char*>(&unknown), settings.unknown_length);
+
+            uint32_t count = 0;
+            if (flags & settings.comp_flag) {
+                
+                unsigned int decompSize;
+                input.read(reinterpret_cast<char*>(&decompSize), 4);
+
+                char * decompData = new char[decompSize];
+                char * compData = new char[dataSize-4];
+
+                input.read(compData, dataSize-4);
+
+                int ret = uncompress((Bytef*)compData, (uLongf*)&decompData, (Bytef*)compData, dataSize-4);
+
+                while (count < decompSize) {
+                    Field field;
+                    count += field.Read(decompData, settings);
+                    fields.push_back(field);
+                }
+
+                count = dataSize;
+            } else {
+                while (count < dataSize) {
+                    Field field;
+                    count += field.Read(input, settings);
+                    fields.push_back(field);
+                }
+            }
+
+            return (uint32_t)settings.length + settings.head_size_length + settings.flag_length + settings.id_length + settings.rev_length + settings.ver_length + settings.unknown_length + count;
+        }
+
+        uint32_t ReadFID(std::ifstream &input, const Settings& settings) {
+            input.read(type, settings.length);
+            input.read(reinterpret_cast<char*>(&dataSize), settings.head_size_length);
+            input.read(reinterpret_cast<char*>(&flags), settings.flag_length);
+            input.read(reinterpret_cast<char*>(&id), settings.id_length);
+
+            //Check distance to end of file.
+            uint32_t pos = input.tellg();
+            input.seekg(0, std::ios_base::end);
+            uint32_t pos2 = input.tellg();
+            if (8 + dataSize <= pos2 - pos)
+                input.seekg(pos + 8 + dataSize, std::ios_base::beg);
+                
+            return uint32_t(24) + dataSize;
+        }
+    };
+
+    struct Group {
+        char type[4];
+        
+        uint32_t groupSize;
+
+        std::vector<uint32_t> formids;
+
+        uint32_t ReadFIDs(std::ifstream &input, const Settings& settings) {
+            try {
+                input.read(type, settings.length);
+            } catch (std::ifstream::failure &e) {
+                return 0;
+            }
+            input.read(reinterpret_cast<char*>(&groupSize), settings.group_size_length);
+
+            uint32_t count = settings.length + settings.group_type_length + settings.group_stamp_length + settings.unknown_length * 2 + settings.ver_length;
+
+            input.seekg(count, std::ios_base::cur);
+
+            count += settings.length + settings.group_size_length;
+
+            while (count < groupSize && input.good()) {
+                char temp[4];
+
+                input.read(temp, settings.length);
+
+                for (int i=0; i < settings.length; ++i)
+                    input.unget();
+
+                if (strncmp(temp,settings.group.data(), 4) == 0) {
+                    Group subgroup;
+                    count += subgroup.ReadFIDs(input, settings);
+                    formids.insert(formids.end(), subgroup.formids.begin(), subgroup.formids.end());
+                } else {
+                    Record record;
+                    count += record.ReadFID(input, settings);
+                    formids.push_back(record.id);
+
+  //                  std::cout << std::string(record.type, 4) << '\t' << std::hex << record.id << std::dec << std::endl;
+                }
+            }
+
+          /*  uint32_t pos = input.tellg();
+            input.seekg(0, std::ios_base::end);
+            uint32_t pos2 = input.tellg();
+            input.seekg(pos, std::ios_base::beg);
+
+            if (pos2 - pos == 0)
+                input.get();
+
+            std::cout << int32_t(count - groupSize) << std::endl
+                      << int32_t(pos2 - pos) << std::endl
+                      << input.eof() << std::endl;*/
+
+            return count;
+        }
+    };
+
+    struct File : public Record {  // Inherited record is TES4.
+        std::vector<uint32_t> formids;
+
+        void Read(std::ifstream &input, const Settings& settings) {
+                
+            Record::Read(input, settings);
+
+            while (input.good()) {
+                Group group;
+                group.ReadFIDs(input, settings);
+                
+                formids.insert(formids.end(), group.formids.begin(), group.formids.end());
+            }
+            
+        }
+
+        void ReadHeader(std::ifstream &input, const Settings& settings) {
+            Record::Read(input, settings);
+        }
+
+        std::vector<uint32_t> GetFormIDs() const {
+            return formids;
+        }
+
+        bool IsMaster(const Settings& settings) const {
+            return (flags & settings.mast_flag);
+        }
+
+        std::vector<std::string> GetMasters() const {
+            std::vector<std::string> masters;
+            for(size_t i=0,max=fields.size(); i < max; ++i){
+                if (strncmp(fields[i].type,"MAST", 4) == 0)
+                    masters.push_back(fields[i].data);
+            }
+            return masters;
+        }
+    };
+
+}
+
+#endif
