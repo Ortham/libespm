32018c9f2b9872cf4abe6eb2309100b3562dc159
WrinklyNinja <wrinklyninja1@gmail.com>
2013-05-06 21:02:16 +0100

    Re-implemented full plugin parsing.

--- src/playground.h
+++ src/playground.h
@@ -30,9 +30,27 @@
 #include <yaml-cpp/yaml.h>
 #include <zlib.h>
 
-//Libespm playground for BOSSv3. Useful things will be implemented properly into my libespm fork.
-//Try to get full plugin data read implemented with reasonable performance (speed/memory) impact - mem usage of 2x plugin filesizes is upper limit.
-//Also test for Oblivion, FO3 and FNV plugins, needs to work for those too.
+/*
+    Libespm playground for BOSSv3. Useful things will be implemented properly into my libespm fork.
+    Try to get full plugin data read implemented with reasonable performance (speed/memory) impact - mem usage of 2x plugin filesizes is upper limit.
+
+    Inter-game differences:
+
+        - Oblivion records have no version or unknown field.
+        - Oblivion groups's stamp is 4 bytes long and groups have no version, unknown1 or unknown2 fields.
+        - Morrowind has no groups at all.
+        - Morrowind records have a 4 byte unknown field inserted before the flags field, and no id, revision, version or unknown fields following it.
+
+    Need to implement the following libespm functionality:
+
+        - getRecords()
+        - getRecordByFieldD - checks type, size and data.
+        - getRecordByID - checks id.
+        - getGroupByName - checks type
+        - getGroups
+
+    
+*/
 
 namespace espm {
     
@@ -112,7 +130,7 @@ namespace espm {
     };
 
     struct Field {
-        Field() : data(NULL) {}
+        Field() : data(NULL), dataSize(0) {}
         Field(const Field& field) {
             dataSize = field.dataSize;
             data = new char[dataSize];
@@ -124,8 +142,8 @@ namespace espm {
             delete [] data;
         }
 
-        Field& operator = (Field const& rhs) {
-            if (data != rhs.data) {
+        Field& operator = (const Field& rhs) {
+            if (this != &rhs) {
                 delete [] data;
 
                 dataSize = rhs.dataSize;
@@ -139,14 +157,19 @@ namespace espm {
         
         char type[4];
 
-        uint16_t dataSize;
+        uint32_t dataSize;  //Usually 16 bit, but 32 bit needed for XXXX-extended subrecords.
 
         char * data;
 
-        uint32_t Read(std::ifstream &input, const Settings& settings) {
+        uint32_t Read(std::ifstream &input, const Settings& settings, const Field& lastField) {
 
             input.read(type, settings.length);
             input.read(reinterpret_cast<char*>(&dataSize), settings.field_size_length);
+
+            if (strncmp(lastField.type,"XXXX", 4) == 0)
+                memcpy(&dataSize, lastField.data, 4);
+
+    //        std::cout << "Subrecord Type: " << std::string(type, 4) << std::endl;
 
             data = new char[dataSize];
             input.read(data, dataSize);
@@ -154,13 +177,19 @@ namespace espm {
             return (uint32_t)settings.length + settings.field_size_length + dataSize;
         }
 
-        uint32_t Read(char * buffer, const Settings& settings) {
+        uint32_t Read(char * buffer, const Settings& settings, const Field& lastField) {
 
             memcpy(type, buffer, settings.length);
             memcpy(&dataSize, buffer + settings.length, settings.field_size_length);
+            
+            if (strncmp(lastField.type,"XXXX", 4) == 0)
+                memcpy(&dataSize, lastField.data, 4);
+
+    //        std::cout << "Subrecord Type: " << std::string(type, 4) << std::endl;
 
             data = new char[dataSize];
             memcpy(data, buffer + settings.length + settings.field_size_length, dataSize);
+
             
             return (uint32_t)settings.length + settings.field_size_length + dataSize;
         }
@@ -179,6 +208,10 @@ namespace espm {
 
         std::vector<Field> fields;
 
+        bool isCompressed(const Settings& settings) const {
+            return flags & settings.comp_flag;
+        }
+
         uint32_t Read(std::ifstream &input, const Settings& settings) {
             input.read(type, settings.length);
             input.read(reinterpret_cast<char*>(&dataSize), settings.head_size_length);
@@ -188,30 +221,50 @@ namespace espm {
             input.read(reinterpret_cast<char*>(&version), settings.ver_length);
             input.read(reinterpret_cast<char*>(&unknown), settings.unknown_length);
 
+   //         std::cout << "Record Type: " << std::string(type, 4) << std::endl;
+   //         std::cout << "FormID: " << std::hex << id << std::dec << std::endl;
+   //         std::cout << "Is Compressed: " << (flags & settings.comp_flag) << std::endl;
+
             uint32_t count = 0;
-            if (flags & settings.comp_flag) {
+            if (isCompressed(settings)) {
+
+                uint32_t compSize = dataSize - 4;
+                uint32_t decompSize;
                 
-                unsigned int decompSize;
                 input.read(reinterpret_cast<char*>(&decompSize), 4);
 
                 char * decompData = new char[decompSize];
-                char * compData = new char[dataSize-4];
+                char * compData = new char[compSize];
+                
+                input.read(compData, compSize);
 
-                input.read(compData, dataSize-4);
-
-                int ret = uncompress((Bytef*)compData, (uLongf*)&decompData, (Bytef*)compData, dataSize-4);
+                int ret = uncompress((Bytef*)decompData, (uLongf*)&decompSize, (Bytef*)compData, (uLong)compSize);
 
                 while (count < decompSize) {
-                    Field field;
-                    count += field.Read(decompData, settings);
+                    Field field, last;
+                    if (!fields.empty())
+                        last = fields.back();
+                    else
+                        memcpy(last.type, "DMMY", 4);  //Initialise the type so nothing weird happens when it gets compared.
+                    count += field.Read(decompData + count, settings, last);
                     fields.push_back(field);
                 }
 
+ /*               uint32_t pos = input.tellg();
+                input.seekg(0, std::ios_base::end);
+                uint32_t pos2 = input.tellg();
+                if (dataSize <= pos2 - pos)
+                    input.seekg(pos + dataSize, std::ios_base::beg);
+*/
                 count = dataSize;
             } else {
                 while (count < dataSize) {
-                    Field field;
-                    count += field.Read(input, settings);
+                    Field field, last;
+                    if (!fields.empty())
+                        last = fields.back();
+                    else
+                        memcpy(last.type, "DMMY", 4);  //Initialise the type so nothing weird happens when it gets compared.
+                    count += field.Read(input, settings, last);
                     fields.push_back(field);
                 }
             }
@@ -238,10 +291,58 @@ namespace espm {
 
     struct Group {
         char type[4];
+        char label[4];
         
         uint32_t groupSize;
+        uint32_t groupType;
+
+        uint16_t stamp;
+        uint16_t unknown1;
+        uint16_t version;
+        uint16_t unknown2;
 
         std::vector<uint32_t> formids;
+        std::vector<Record> records;
+        std::vector<Group> subgroups;
+
+        uint32_t Read(std::ifstream &input, const Settings& settings) {
+            try {
+                input.read(type, settings.length);
+            } catch (std::ifstream::failure &e) {
+                return 0;
+            }
+            input.read(reinterpret_cast<char*>(&groupSize), settings.group_size_length);
+            input.read(label, settings.length);
+            input.read(reinterpret_cast<char*>(&groupType), settings.group_type_length);
+            input.read(reinterpret_cast<char*>(&stamp), settings.group_stamp_length);
+            input.read(reinterpret_cast<char*>(&unknown1), settings.unknown_length);
+            input.read(reinterpret_cast<char*>(&version), settings.ver_length);
+            input.read(reinterpret_cast<char*>(&unknown2), settings.unknown_length);
+
+            uint32_t count = settings.length * 2 + settings.group_size_length + settings.group_type_length + settings.group_stamp_length + settings.unknown_length * 2 + settings.ver_length;
+
+            while (count < groupSize && input.good()) {
+                char temp[4];
+
+                input.read(temp, settings.length);
+
+                for (int i=0; i < settings.length; ++i)
+                    input.unget();
+
+                if (strncmp(temp,settings.group.data(), 4) == 0) {
+                    Group subgroup;
+                    count += subgroup.Read(input, settings);
+                    subgroups.push_back(subgroup);
+                } else {
+                    Record record;
+                    count += record.Read(input, settings);
+                    records.push_back(record);
+                }
+            }
+
+            return count;
+
+        }
 
         uint32_t ReadFIDs(std::ifstream &input, const Settings& settings) {
             try {
@@ -273,31 +374,43 @@ namespace espm {
                     Record record;
                     count += record.ReadFID(input, settings);
                     formids.push_back(record.id);
-
-  //                  std::cout << std::string(record.type, 4) << '\t' << std::hex << record.id << std::dec << std::endl;
                 }
             }
 
-          /*  uint32_t pos = input.tellg();
-            input.seekg(0, std::ios_base::end);
-            uint32_t pos2 = input.tellg();
-            input.seekg(pos, std::ios_base::beg);
-
-            if (pos2 - pos == 0)
-                input.get();
-
-            std::cout << int32_t(count - groupSize) << std::endl
-                      << int32_t(pos2 - pos) << std::endl
-                      << input.eof() << std::endl;*/
-
             return count;
+        }
+
+        std::vector<uint32_t> GetFormIDs() {
+            if (formids.empty()) {
+                for (int i=0,max=subgroups.size(); i < max; ++i) {
+                    std::vector<uint32_t> fids = subgroups[i].GetFormIDs();
+                    formids.insert(formids.end(), fids.begin(), fids.end());
+                }
+                for (int i=0,max=records.size(); i < max; ++i) {
+                    formids.push_back(records[i].id);
+                }
+            }
+            return formids;
         }
     };
 
     struct File : public Record {  // Inherited record is TES4.
         std::vector<uint32_t> formids;
+        std::vector<Group> groups;
 
         void Read(std::ifstream &input, const Settings& settings) {
+                
+            Record::Read(input, settings);
+
+            while (input.good()) {
+                Group group;
+                group.Read(input, settings);
+                groups.push_back(group);
+            }
+            
+        }
+
+        void ReadFIDs(std::ifstream &input, const Settings& settings) {
                 
             Record::Read(input, settings);
 
@@ -314,7 +427,13 @@ namespace espm {
             Record::Read(input, settings);
         }
 
-        std::vector<uint32_t> GetFormIDs() const {
+        std::vector<uint32_t> GetFormIDs() {
+            if (formids.empty()) {
+                for (int i=0,max=groups.size(); i < max; ++i) {
+                    std::vector<uint32_t> fids = groups[i].GetFormIDs();
+                    formids.insert(formids.end(), fids.begin(), fids.end());
+                }
+            }
             return formids;
         }
 
