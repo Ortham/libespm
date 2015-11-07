151779e93fbe179c5e8217517af930dcbaa02da0
Oliver Hamlet <oliver.hamlet@gmail.com>
2015-11-07 09:54:54 +0000

    Refactor Group class
    Similar to the Record class refactor in
    f65dca6dd2795dc403a36187e5dd0a21831457b0.
    
    * Don't store the group size since it's only used during parsing.
    * Use a string instead of a char array when checking the record type,
    since it's easier to read.
    * Simplify record reading loop to have fewer stream position
    calculations.
    * Make Group reading detail members private.
    * Remove a magic number
    
    Use static member variable for group type string

--- include/libespm2/Group.h
+++ include/libespm2/Group.h
@@ -28,56 +28,59 @@
 namespace libespm2 {
   class Group {
   private:
-    uint32_t groupSize;
     std::set<uint32_t> _formIds;
 
+    static const std::string groupType;
     static const int typeLength = 4;
   public:
-    inline Group() : groupSize(0) {}
-
     inline void read(std::istream& input) {
-      readHeader(input);
-      readRecords(input);
+      uint32_t totalRecordsSize = readHeader(input);
+      readRecords(input, totalRecordsSize);
     }
 
-    inline void readHeader(std::istream& input) {
+    inline std::set<uint32_t> getRecordFormIds() const {
+      return _formIds;
+    }
+
+  private:
+    inline uint32_t readHeader(std::istream& input) {
+      uint32_t groupSize = 0;
+
       // Check the input stream is large enough.
-      size_t totalHeaderLength = 4 + sizeof(groupSize) + 16;
+      size_t totalHeaderLength = typeLength + sizeof(groupSize) + 16;
       if (!isStreamLongEnough(input, totalHeaderLength))
         throw std::runtime_error("Invalid plugin: incomplete group header found.");
 
       // Read in the record type.
-      char type[typeLength];
-      input.read(type, typeLength);
-      if (memcmp(type, "GRUP", typeLength) != 0)
-        throw std::runtime_error("Invalid plugin: group with type \"" + std::string(type, 4) + "\" found.");
+      std::string type;
+      type.resize(typeLength);
+      input.read(reinterpret_cast<char*>(&type[0]), typeLength);
+      if (type != groupType)
+        throw std::runtime_error("Invalid plugin: group with type \"" + type + "\" found.");
 
       // Read the total group size.
       input.read(reinterpret_cast<char*>(&groupSize), sizeof(groupSize));
 
       // Skip to the end of the header.
       input.seekg(16, std::ios_base::cur);
+
+      return groupSize - 24;
     }
 
-    inline void readRecords(std::istream& input) {
-      uint32_t totalRecordsSize = groupSize - 24;
+    inline void readRecords(std::istream& input, uint32_t totalRecordsSize) {
       if (!isStreamLongEnough(input, totalRecordsSize))
         throw std::runtime_error("Invalid plugin: incomplete group records found.");
 
-      size_t bytesRead = 0;
-      while (bytesRead < totalRecordsSize) {
+      std::streampos startingInputPos = input.tellg();
+      while (input.tellg() - startingInputPos < totalRecordsSize) {
         Record record;
-        std::streampos beforePos = input.tellg();
         record.read(input);
         _formIds.insert(record.getFormId());
-        bytesRead += input.tellg() - beforePos;
       }
     }
-
-    inline std::set<uint32_t> getRecordFormIds() const {
-      return _formIds;
-    }
   };
+
+  const std::string Group::groupType = "GRUP";
 }
 
 #endif
